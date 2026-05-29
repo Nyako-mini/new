c5d249f685f24e6c0f54f650055e4f6a21dd28da
Nyako_Desu <shuyue114514@gmail.com>
2026-05-29 17:55:15 +0800

    Update batteryinfo.c

--- kernel/batteryinfo.c
+++ kernel/batteryinfo.c
@@ -11,7 +11,6 @@
 #define SRC_CYCLE "/sys/class/power_supply/battery/cycle_count"
 
 static struct class *fuel_class;
-static struct device *fuel_device;
 
 static ssize_t read_source_file(const char *path, char *buf, size_t count)
 {
@@ -59,35 +58,36 @@ static ssize_t cycle_show(struct class *cls, struct class_attribute *attr, char 
 static CLASS_ATTR_RO(soh);
 static CLASS_ATTR_RO(cycle);
 
-static struct attribute *fuel_attrs[] = {
-    &class_attr_soh.attr,
-    &class_attr_cycle.attr,
-    NULL,
-};
-ATTRIBUTE_GROUPS(fuel);
-
 static int __init fuelsummary_init(void)
 {
+    int ret;
+
     fuel_class = class_create(THIS_MODULE, "fuelsummary");
     if (IS_ERR(fuel_class))
         return PTR_ERR(fuel_class);
 
-    fuel_device = device_create(fuel_class, NULL, MKDEV(0, 0), NULL, "battery");
-    if (IS_ERR(fuel_device)) {
-        class_destroy(fuel_class);
-        return PTR_ERR(fuel_device);
-    }
+    ret = class_create_file(fuel_class, &class_attr_soh);
+    if (ret)
+        goto err_soh;
 
-    fuel_class->dev_groups = fuel_groups;
+    ret = class_create_file(fuel_class, &class_attr_cycle);
+    if (ret)
+        goto err_cycle;
+
     return 0;
+
+err_cycle:
+    class_remove_file(fuel_class, &class_attr_soh);
+err_soh:
+    class_destroy(fuel_class);
+    return ret;
 }
 
 static void __exit fuelsummary_exit(void)
 {
-    if (fuel_device)
-        device_destroy(fuel_class, MKDEV(0, 0));
-    if (fuel_class)
-        class_destroy(fuel_class);
+    class_remove_file(fuel_class, &class_attr_cycle);
+    class_remove_file(fuel_class, &class_attr_soh);
+    class_destroy(fuel_class);
 }
 
 module_init(fuelsummary_init);
