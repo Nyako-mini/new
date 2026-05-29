909aef054e8d911189e8ab3f86474f326dc82d3c
Nyako_Desu <shuyue114514@gmail.com>
2026-05-29 18:23:45 +0800

    Update cpuinfo.c

--- kernel/cpuinfo.c
+++ kernel/cpuinfo.c
@@ -3,6 +3,8 @@
 #include <linux/module.h>
 #include <linux/sysfs.h>
 #include <linux/kobject.h>
+#include <linux/fs.h>
+#include <linux/device.h>
 
 #define DRIVER_NAME "vivo_cpu_info"
 
@@ -32,16 +34,16 @@ static struct cpu_config normal = {
 
 static struct cpu_config *cur = &high_perf;
 
-static struct kobject *cpu_info_kobj = NULL;
-static struct kobject *devices_kobj = NULL;
-static struct kobject *soc1_kobj = NULL;
+static struct class *cpu_class = NULL;
+static struct device *cpu_device = NULL;
+static struct device *soc1_device = NULL;
 
-static ssize_t type_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+static ssize_t type_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
     return scnprintf(buf, PAGE_SIZE, "%d\n", cur->type);
 }
 
-static ssize_t type_store(struct kobject *kobj, struct kobj_attribute *attr,
+static ssize_t type_store(struct device *dev, struct device_attribute *attr,
                           const char *buf, size_t count)
 {
     int ret, val;
@@ -59,155 +61,166 @@ static ssize_t type_store(struct kobject *kobj, struct kobj_attribute *attr,
     return count;
 }
 
-static ssize_t cpu_freq_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+static ssize_t cpu_freq_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
     return scnprintf(buf, PAGE_SIZE, "%d\n", cur->freq_khz);
 }
 
-static ssize_t core_num_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+static ssize_t core_num_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
     return scnprintf(buf, PAGE_SIZE, "%d\n", cur->cores);
 }
 
-static ssize_t cpu_set_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+static ssize_t cpu_set_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
     return scnprintf(buf, PAGE_SIZE, "%s\n", cur->model);
 }
 
-static ssize_t cpu_type_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+static ssize_t cpu_type_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
     return scnprintf(buf, PAGE_SIZE, "%s\n", cur->model);
 }
 
-static ssize_t user_cpu_freq_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+static ssize_t user_cpu_freq_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
     return scnprintf(buf, PAGE_SIZE, "%s\n", cur->user_freq);
 }
 
-static struct kobj_attribute type_attr = __ATTR(type, 0644, type_show, type_store);
-static struct kobj_attribute cpu_freq_attr = __ATTR(cpu_freq, 0444, cpu_freq_show, NULL);
-static struct kobj_attribute core_num_attr = __ATTR(core_num, 0444, core_num_show, NULL);
-static struct kobj_attribute cpu_set_attr = __ATTR(cpu_set, 0444, cpu_set_show, NULL);
-static struct kobj_attribute cpu_type_attr = __ATTR(cpu_type, 0444, cpu_type_show, NULL);
-static struct kobj_attribute user_cpu_freq_attr = __ATTR(user_cpu_freq, 0444, user_cpu_freq_show, NULL);
+static DEVICE_ATTR(type, 0644, type_show, type_store);
+static DEVICE_ATTR(cpu_freq, 0444, cpu_freq_show, NULL);
+static DEVICE_ATTR(core_num, 0444, core_num_show, NULL);
+static DEVICE_ATTR(cpu_set, 0444, cpu_set_show, NULL);
+static DEVICE_ATTR(cpu_type, 0444, cpu_type_show, NULL);
+static DEVICE_ATTR(user_cpu_freq, 0444, user_cpu_freq_show, NULL);
 
 static int __init vivo_cpu_info_init(void)
 {
     int ret;
 
-    cpu_info_kobj = kobject_create_and_add("cpu_info", NULL);
-    if (!cpu_info_kobj) {
-        return -ENOMEM;
+    cpu_class = class_create(THIS_MODULE, "cpu_info");
+    if (IS_ERR(cpu_class)) {
+        return PTR_ERR(cpu_class);
     }
 
-    ret = sysfs_create_file(cpu_info_kobj, &type_attr.attr);
-    if (ret)
-        goto err_cpu_info_type;
-
-    ret = sysfs_create_file(cpu_info_kobj, &cpu_freq_attr.attr);
-    if (ret)
-        goto err_cpu_info_freq;
-
-    ret = sysfs_create_file(cpu_info_kobj, &core_num_attr.attr);
-    if (ret)
-        goto err_cpu_info_core;
-
-    ret = sysfs_create_file(cpu_info_kobj, &cpu_set_attr.attr);
-    if (ret)
-        goto err_cpu_info_set;
-
-    ret = sysfs_create_file(cpu_info_kobj, &user_cpu_freq_attr.attr);
-    if (ret)
-        goto err_cpu_info_user;
-
-    devices_kobj = kobject_create_and_add("devices", NULL);
-    if (!devices_kobj) {
-        ret = -ENOMEM;
-        goto err_devices;
+    cpu_device = device_create(cpu_class, NULL, MKDEV(0, 0), NULL, "info");
+    if (IS_ERR(cpu_device)) {
+        ret = PTR_ERR(cpu_device);
+        goto err_cpu_device;
     }
 
-    soc1_kobj = kobject_create_and_add("soc1", devices_kobj);
-    if (!soc1_kobj) {
-        ret = -ENOMEM;
-        goto err_soc1;
+    ret = device_create_file(cpu_device, &dev_attr_type);
+    if (ret)
+        goto err_type;
+
+    ret = device_create_file(cpu_device, &dev_attr_cpu_freq);
+    if (ret)
+        goto err_freq;
+
+    ret = device_create_file(cpu_device, &dev_attr_core_num);
+    if (ret)
+        goto err_core;
+
+    ret = device_create_file(cpu_device, &dev_attr_cpu_set);
+    if (ret)
+        goto err_set;
+
+    ret = device_create_file(cpu_device, &dev_attr_user_cpu_freq);
+    if (ret)
+        goto err_user;
+
+    ret = device_create_file(cpu_device, &dev_attr_cpu_type);
+    if (ret)
+        goto err_cpu_type;
+
+    device_remove_file(cpu_device, &dev_attr_cpu_type);
+    device_remove_file(cpu_device, &dev_attr_user_cpu_freq);
+    device_remove_file(cpu_device, &dev_attr_cpu_set);
+    device_remove_file(cpu_device, &dev_attr_core_num);
+    device_remove_file(cpu_device, &dev_attr_cpu_freq);
+    device_remove_file(cpu_device, &dev_attr_type);
+    device_destroy(cpu_class, MKDEV(0, 0));
+    class_destroy(cpu_class);
+    
+    cpu_class = class_create(THIS_MODULE, "devices");
+    if (IS_ERR(cpu_class)) {
+        return PTR_ERR(cpu_class);
     }
 
-    ret = sysfs_create_file(soc1_kobj, &type_attr.attr);
+    soc1_device = device_create(cpu_class, NULL, MKDEV(0, 0), NULL, "soc1");
+    if (IS_ERR(soc1_device)) {
+        ret = PTR_ERR(soc1_device);
+        goto err_soc1_device;
+    }
+
+    ret = device_create_file(soc1_device, &dev_attr_type);
     if (ret)
         goto err_soc1_type;
 
-    ret = sysfs_create_file(soc1_kobj, &cpu_freq_attr.attr);
+    ret = device_create_file(soc1_device, &dev_attr_cpu_freq);
     if (ret)
         goto err_soc1_freq;
 
-    ret = sysfs_create_file(soc1_kobj, &core_num_attr.attr);
+    ret = device_create_file(soc1_device, &dev_attr_core_num);
     if (ret)
         goto err_soc1_core;
 
-    ret = sysfs_create_file(soc1_kobj, &cpu_type_attr.attr);
+    ret = device_create_file(soc1_device, &dev_attr_cpu_type);
     if (ret)
         goto err_soc1_cpu_type;
 
-    ret = sysfs_create_file(soc1_kobj, &user_cpu_freq_attr.attr);
+    ret = device_create_file(soc1_device, &dev_attr_user_cpu_freq);
     if (ret)
         goto err_soc1_user;
 
     return 0;
 
 err_soc1_user:
-    sysfs_remove_file(soc1_kobj, &cpu_type_attr.attr);
+    device_remove_file(soc1_device, &dev_attr_cpu_type);
 err_soc1_cpu_type:
-    sysfs_remove_file(soc1_kobj, &core_num_attr.attr);
+    device_remove_file(soc1_device, &dev_attr_core_num);
 err_soc1_core:
-    sysfs_remove_file(soc1_kobj, &cpu_freq_attr.attr);
+    device_remove_file(soc1_device, &dev_attr_cpu_freq);
 err_soc1_freq:
-    sysfs_remove_file(soc1_kobj, &type_attr.attr);
+    device_remove_file(soc1_device, &dev_attr_type);
 err_soc1_type:
-    kobject_put(soc1_kobj);
-    soc1_kobj = NULL;
-err_soc1:
-    kobject_put(devices_kobj);
-    devices_kobj = NULL;
-err_devices:
-    sysfs_remove_file(cpu_info_kobj, &user_cpu_freq_attr.attr);
-err_cpu_info_user:
-    sysfs_remove_file(cpu_info_kobj, &cpu_set_attr.attr);
-err_cpu_info_set:
-    sysfs_remove_file(cpu_info_kobj, &core_num_attr.attr);
-err_cpu_info_core:
-    sysfs_remove_file(cpu_info_kobj, &cpu_freq_attr.attr);
-err_cpu_info_freq:
-    sysfs_remove_file(cpu_info_kobj, &type_attr.attr);
-err_cpu_info_type:
-    kobject_put(cpu_info_kobj);
-    cpu_info_kobj = NULL;
+    device_destroy(cpu_class, MKDEV(0, 0));
+err_soc1_device:
+    class_destroy(cpu_class);
+    return ret;
+
+err_cpu_type:
+    device_remove_file(cpu_device, &dev_attr_user_cpu_freq);
+err_user:
+    device_remove_file(cpu_device, &dev_attr_cpu_set);
+err_set:
+    device_remove_file(cpu_device, &dev_attr_core_num);
+err_core:
+    device_remove_file(cpu_device, &dev_attr_cpu_freq);
+err_freq:
+    device_remove_file(cpu_device, &dev_attr_type);
+err_type:
+    device_destroy(cpu_class, MKDEV(0, 0));
+err_cpu_device:
+    class_destroy(cpu_class);
     return ret;
 }
 
 static void __exit vivo_cpu_info_exit(void)
 {
-    if (soc1_kobj) {
-        sysfs_remove_file(soc1_kobj, &user_cpu_freq_attr.attr);
-        sysfs_remove_file(soc1_kobj, &cpu_type_attr.attr);
-        sysfs_remove_file(soc1_kobj, &core_num_attr.attr);
-        sysfs_remove_file(soc1_kobj, &cpu_freq_attr.attr);
-        sysfs_remove_file(soc1_kobj, &type_attr.attr);
-        kobject_put(soc1_kobj);
+    if (soc1_device) {
+        device_remove_file(soc1_device, &dev_attr_user_cpu_freq);
+        device_remove_file(soc1_device, &dev_attr_cpu_type);
+        device_remove_file(soc1_device, &dev_attr_core_num);
+        device_remove_file(soc1_device, &dev_attr_cpu_freq);
+        device_remove_file(soc1_device, &dev_attr_type);
+        device_destroy(cpu_class, MKDEV(0, 0));
     }
-    if (devices_kobj) {
-        kobject_put(devices_kobj);
-    }
-    if (cpu_info_kobj) {
-        sysfs_remove_file(cpu_info_kobj, &user_cpu_freq_attr.attr);
-        sysfs_remove_file(cpu_info_kobj, &cpu_set_attr.attr);
-        sysfs_remove_file(cpu_info_kobj, &core_num_attr.attr);
-        sysfs_remove_file(cpu_info_kobj, &cpu_freq_attr.attr);
-        sysfs_remove_file(cpu_info_kobj, &type_attr.attr);
-        kobject_put(cpu_info_kobj);
+    if (cpu_class) {
+        class_destroy(cpu_class);
     }
 }
 
-pure_initcall(vivo_cpu_info_init);
+late_initcall(vivo_cpu_info_init);
 module_exit(vivo_cpu_info_exit);
 
 MODULE_LICENSE("GPL");
